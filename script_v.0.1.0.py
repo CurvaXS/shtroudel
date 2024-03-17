9cc58c25d360cd32aae54d36259a8b1b1a32a5ca
CurvaXS <suxarik24@gmail.com>
2024-03-17 05:37:39 +0300

    without_proxy

--- script_v.0.1.0.py
+++ script_v.0.1.0.py
@@ -10,7 +10,6 @@ import subprocess
 
 """
 """
-#temporary_number_of_sessions = 0
 #chrome_driver = input("Введите путь на драйвер хрома (абсолютаная ссылка): ")
 serv = webdriver.ChromeService(executable_path=r'.\chromedriver.exe') 
 opt1 = Options()
@@ -36,6 +35,7 @@ def single(i, proxy):
     opt1.add_experimental_option('debuggerAddress', f'localhost:100{i}')
     driver.get('https://drgn4r.casino/bonus')
     time.sleep(9999)
+    
  
 
 def multi(session_quantity):
@@ -49,7 +49,6 @@ def multi(session_quantity):
         driver = webdriver.Chrome(service=serv, options=opt1)
         driver.get("https://drgn4r.casino/bonus")
         k -= 1
-    time.sleep(1)
 
 def input_code(code, session_quantity):
     k=session_quantity
@@ -59,7 +58,7 @@ def input_code(code, session_quantity):
             print(f'localhost:100{i}')
         
         driver = webdriver.Chrome(service=serv, options=opt1)
-        code_input = driver.find_element(By.CSS_SELECTOR, value='[placeholder="Введите код"]')
+        code_input = driver.find_element(By.CSS_SELECTOR, '[placeholder="Введите код"]')
         code_input.clear()
         code_input.send_keys(f'{code}')
         code_button = driver.find_element(
@@ -69,42 +68,49 @@ def input_code(code, session_quantity):
         )
         code_button.click()
         
-        time.sleep(2)
-        parent_recaptcha = driver.find_element(
-            By.XPATH,
-            '//div[@class="relative inline-block w-full whitespace-normal rounded-md bg-bg-wrapper text-left align-middle text-base shadow-modal transition-[max-width] max-w-md"]'
-            #By.ID, value='[id="g-recaptcha-response-2"]'
-            #'//textarea[@id="g-recaptcha-response-2"]'
-            #"//div[@class='flex items-center justify-between border-b border-border p-3']/h3[text() = 'Пройдите проверку']"
-            #"//div[@class='rc-anchor-content']/div[1]/div/div/span"
-            #//div[@class='vfm vfm--fixed vfm--inset']/[@class='vfm__overlay vfm--overlay vfm--absolute vfm--inset vfm--prevent-none bg-[rgb(0_0_0/0.35)]']/div[@class='vfm__content vfm--outline-none']/div/div/
-        )
-        recaptcha = parent_recaptcha.find_element(
-            By.XPATH,
-            './/iframe'
-            #'.//div[@class="p-3 lg:p-4"]/div/div/div/iframe/'
-            #/div[@class="flex items-center justify-between border-b border-border p-3"
-        )
+        time.sleep(0.04)
 
-        #print(recaptcha)
+        iframe = driver.find_element(By.XPATH, 
+            '//div[@class="relative inline-block w-full whitespace-normal rounded-md bg-bg-wrapper text-left align-middle text-base shadow-modal transition-[max-width] max-w-md"]/div[2]/div/div/div/iframe'
+       )
+        print(iframe)
+        
+        driver.switch_to.frame(iframe)
+        print('PIZDEC')
+        recapt = driver.find_element(By.XPATH, '//span[@id="recaptcha-anchor"]')
+        recapt.click()
+        print(recapt)
+        #driver.switch_to.default_content()
         
         k -= 1
 
-    time.sleep(1)
+    #time.sleep(1)
 
 
 def path_to_chromeprofiles():
+    print("Если у вас windows 10 - Введите 10")
+    print("Если у вас windows 11 - Введите 11")
+    print("")
+    win_choise = int(input("Введите ответ: "))
+    print("")
+
+    if (win_choise == 10):
+        chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    elif (win_choise == 11):
+        chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
     fff = input("Введите путь на профили хрома: ")
     kol = input("Введите кол-во сессий: ")
+
     #Пример:
     #fff = r'D:\donttch\Mock-chrome\chromeprofile_'
-    chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    
+    
     num_instances = int(kol)
     #кол-во окон (рекомедн. 1-10):
     for i in range(num_instances):
-        link = fr"{fff}{i}"
+        link = fr"{fff}\profile_{i}"
         subprocess.Popen([chrome_path, f"--remote-debugging-port=100{i}", f"--user-data-dir={link}"])
-            # Add a delay to ensure each instance has time to start
         time.sleep(1)
 
 def main():
